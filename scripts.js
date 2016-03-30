f62474371f671e59fc1e840c7e67d65b51e7ad5a
Daniel Boterhoven <hello@danielboterhoven.tech>
2016-03-30 11:28:28 +0800

    Added solutions

--- scripts.js
+++ scripts.js
@@ -71,16 +71,38 @@ function main() {
     // note that the 'numbers' parameter is a variable of type Array
     function sumNumbers(numbers) {
         
+        // - first create a varialbe 'runningTotal'
+        var runningTotal = 0;
+
+        // - note that the param 'numbers' is an Array so create a for loop similar to line 44 in the isVowel example
+        // - (open loop)
+        for (i = 0; i < numbers.length; i++) {
+            
+            runningTotal = runningTotal + numbers[i];
+            // - we're now iterative over each value in the 'numbers' array so get a reference to the value in this loop cycle
+            // - we did this previously at line 46
+
+            // - now increment the 'runningTotal' variable defined at line 73 with the value we just referenced
+            // - so using the test below this number will in the first iteration be '1', then '2', then '3', then '4'
+            // - or in the second test it will be '3', '3', '4', then '5'
+
+        // - (close loop)
+        }
+
+        // - at this point the 'runningTotal' variable will be the sum of all values which were contained in the array 'numbers'
         
+        // - we now return the 'runningTotal' variable like this...
+        // return runningTotal
+        return runningTotal;
     }
     
     var sum1 = sumNumbers([1, 2, 3, 4]);
     console.log(sum1); // should equal 10
     outputLine(sum1);
     
-    var sum1 = sumNumbers([3, 3, 4, 5]);
-    console.log(sum1); // should equal 15
-    outputLine(sum1);
+    var sum2 = sumNumbers([3, 3, 4, 5]);
+    console.log(sum2); // should equal 15
+    outputLine(sum2);
     
     
     // Homework #2 - reverse
@@ -88,10 +110,18 @@ function main() {
     // note that the 'phrase' parameter is of type String
     function reverse(phrase) {
         
-        // HINT: this converts a String such as 'hello' into the Array ['h', 'e', 'l', 'l', 'o'];
+        var returnString = '';
+        
+        // HINT: this converts a String such as 'hello' into the Array 
+        // ['h', 'e', 'l', 'l', 'o'];
         var characters = phrase.split('');
+
+        for (i = characters.length - 1; i >= 0; i--) {
+            //var character = characters[i];
+            returnString = returnString + characters[i];
+        }
         
-        
+        return returnString;
     }
     
     var reversedCharacters = reverse('abcdef');
@@ -107,6 +137,19 @@ function main() {
     // this function should return an Array containing every second letter contained in the 'phrase' parameter
     function everySecondLetter(phrase) {
         
+        var returnArray = [];
+        
+        var characters = phrase.split('');
+        
+        for (i = 1; i < characters.length; i = i + 2) {
+            
+            var character = characters[i];
+            
+            returnArray.push(character);
+            
+        }
+        
+        return returnArray;
                 
     }
     
